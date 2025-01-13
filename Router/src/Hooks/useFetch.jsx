26b9c292c43721508d7aa7faa9ec9fe293910a25
KaiqueBifulgo <kaiquebwork@hotmail.com>
2025-01-13 02:03:51 -0300

    db e correções

---
+++ Router/src/Hooks/useFetch.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+
+
+// CUSTOM HOOK
+export const useFetch = (url) => {
+ 
+    const [data, setData] = useState(null);
+
+
+    // LOADING
+    const [loading, setLoading] = useState(false);
+
+    // ERROR
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        
+
+        const fetchData = async () => {
+
+            try {
+                 // LOADING
+            setLoading(true);
+
+            const res = await fetch(url);
+            const json = await res.json();
+
+            setLoading(false);
+
+            setData(json);
+            } catch (error) {
+
+                setError("Houve algum erro")
+                console.log(error);
+
+            }
+           
+        }
+
+        fetchData();
+    }, [url])
+
+    return{data, loading, error};
+
+}
--- Router/src/routes/Home.jsx
+++ Router/src/routes/Home.jsx
@@ -1,6 +1,25 @@
+// CARREGANDO DADOS
+import {useFetch} from '../Hooks/useFetch'
+
+const url = "http://localhost:3000/products"
+
 const Home = () => {
+  const {data: items} = useFetch(url);
+
   return (
-    <div>Home</div>
+    <div>
+      <h1>Home</h1>
+      {/* CARREGAMENTO DE DADOS */}
+      <ul className="produtos">
+        {items && items.map((item) => (
+          <li key={item.id}>
+            <h2>{item.item}</h2>
+            <p>R$: {item.preço}</p>
+          </li>
+        ))}
+
+      </ul>
+    </div>
   )
 }
 
